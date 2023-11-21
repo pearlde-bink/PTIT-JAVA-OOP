17c4e277d514c4205adf751a2e3b6b7f7a2a131c
pearlde-bink <tuanbinhnguyenngoc@gmail.com>
2023-11-22 00:08:15 +0700

    add onTapGui

---
+++ src/model/QLMuon.java
@@ -0,0 +1,59 @@
+package model;
+
+import java.io.Serializable;
+
+public class QLMuon implements Serializable{
+    private banDoc bandoc;
+    private Sach sach;
+    private int sluong;
+    private String ttrang;
+    
+    public QLMuon(){};
+
+    public QLMuon(banDoc bandoc, Sach sach, int sluong, String ttrang) {
+        this.bandoc = bandoc;
+        this.sach = sach;
+        this.sluong = sluong;
+        this.ttrang = ttrang;
+    }
+
+    public banDoc getBandoc() {
+        return bandoc;
+    }
+
+    public void setBandoc(banDoc bandoc) {
+        this.bandoc = bandoc;
+    }
+
+    public Sach getSach() {
+        return sach;
+    }
+
+    public void setSach(Sach sach) {
+        this.sach = sach;
+    }
+
+    public int getSluong() {
+        return sluong;
+    }
+
+    public void setSluong(int sluong) {
+        this.sluong = sluong;
+    }
+
+    public String getTtrang() {
+        return ttrang;
+    }
+
+    public void setTtrang(String ttrang) {
+        this.ttrang = ttrang;
+    }
+    
+    public Object[] toObject(){
+        return new Object[]{
+            bandoc.getMa(), bandoc.getHoten(),
+            sach.getMa(), sach.getTen(),
+            sluong, ttrang
+        };
+    }
+}
